3c73a0a69026555623133c56b84aa3a439545834
Blake McCool <blake.mccool@gmail.com>
2015-06-10 11:47:03 -0600

    back to master version of pdfJS

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 if [ ! -d vendor/jsPDF ]; then
-    wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/v1.0.272.zip
+    wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/master.zip
     unzip jsPDF.zip
     mkdir vendor
-    mv jsPDF-1.0.272 vendor/jsPDF
+    mv jsPDF-master vendor/jsPDF
     rm jsPDF.zip
 fi
