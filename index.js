bb6a24d0c01f747886a79064b966cd4e502fd21b
Blake McCool <blake.mccool@gmail.com>
2015-06-10 11:46:28 -0600

    modify plugin path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ jsPDF = require(path+'jspdf').jsPDF;
 //Load all the plugins
 var plugins = ['addhtml', 'addimage', 'annotations', 'autoprint', 'cell', 'from_html', 'javascript', 'outline', 'png_support', 'sillysvgrenderer', 'split_text_to_size', 'standard_fonts_metrics', 'total_pages'];
 plugins.map(function(plugin){
-    require(path+'jspdf.plugin.'+plugin+'.js');
+    require(path+'/plugins/' + plugin + '.js');
 });
 
 //Modify the save function to save to disk
